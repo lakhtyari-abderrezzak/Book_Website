98c484ae3a13e0171ec7147a9dbcd0a7c962adf2
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-19 18:23:42 +0000

    Edit home route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\HomeController;
+use App\Livewire\BookList;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
+Route::get('/', HomeController::class)->name('home');
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
+    
+Route::get('books', [BookController::class , 'index'])->name('books.index');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
