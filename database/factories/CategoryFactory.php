55241255950fac7268a6e5b64b7607cc8f8b78ba
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-23 13:52:21 +0000

    Add category factory to populate database

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->sentence(80),
+            'img_path' => $this->faker->imageUrl(),
+        ];
+    }
+}
