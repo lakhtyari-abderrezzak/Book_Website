cf98cc8e7c42550d04c70103e1298bb496801d32
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-23 13:45:29 +0000

    Edit BookFactroy by adding category id from category factory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,6 +18,7 @@ class BookFactory extends Factory
     public function definition(): array
     {
         return [
+            'category_id' => Category::factory(),
             'title' => $this->faker->sentence(2),
             'author' =>  $this->faker->name(),
             'edition' =>  $this->faker->date(),
