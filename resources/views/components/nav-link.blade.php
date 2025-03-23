b9b1b990c2a0c4e4dc920a0c17d116f82675cd31
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-23 13:49:51 +0000

    Add nav-links component and included a tag

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,11 @@
+@props(['href', 'active' => false])
+<a  
+href=" {{ $href }} "
+{{ $attributes->class([
+    'text-gray-300 hover:text-yellow-400',
+    'text-yellow-400 font-bold' => $active
+]) }}>
+
+{{ $slot }}
+
+</a>
