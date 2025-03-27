e95e4c96d35a4f36956e34c2b02082100b3d67b0
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-27 13:40:08 +0000

    Add wire navigate to nav-link

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,10 @@
 @props(['href', 'active' => false])
 <a  
-href=" {{ $href }} "
+href="{{ $href }}"
 {{ $attributes->class([
     'text-gray-300 hover:text-yellow-400',
     'text-yellow-400 font-bold' => $active
-]) }}>
+]) }} wire:navigate>
 
 {{ $slot }}
-
 </a>
