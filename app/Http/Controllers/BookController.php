5c75766b429ce3840571184b102aff993edd5715
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-01 01:24:46 +0100

    Add show function with book variable that returns books,show view

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index(){
         return view('books.index');
+    }
+    public function show(Book $book){
+        $book = Book::find($book);
+        return view('books.show', compact('book'));
     }
 }
