e39ffef5fe4d2ac4d796873ef7bc25da51b433d5
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-01 01:25:51 +0100

    Add show function to categoriesController with $category returning categories.show view

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function show($id){
+        $category = Category::with('books')->findOrFail($id);
+        return view('categories.show', compact('category'));
+    }
 }
