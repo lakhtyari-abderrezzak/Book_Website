02cc054a5ac69792e54b3d7ea6a2f9d689baa620
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-01 01:23:52 +0100

    Add tesimonials and bookfacts variables to the index function

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Contact;
+use App\Models\Fact;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
     public function index(){
+        $testimonials = Contact::take(4)->get();
         $categories = Category::take(9)->get();
+        $bookFacts = Fact::take(3)->get();
         return view('about', [
-            'categories' => $categories
+            'categories' => $categories,
+            'testimonials' => $testimonials,
+            'bookFacts' => $bookFacts
         ]);
     }
 }
