93f58f38915e7474416fc66bd311f41cfd27abbb
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-03 19:04:59 +0100

    Edit $categories to take(9) and get latest categories

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -11,7 +11,7 @@ class AboutController extends Controller
 {
     public function index(){
         $testimonials = Contact::take(4)->get();
-        $categories = Category::take(9)->get();
+        $categories = Category::take(9)->latest()->get();
         $bookFacts = Fact::take(3)->get();
         return view('about', [
             'categories' => $categories,
