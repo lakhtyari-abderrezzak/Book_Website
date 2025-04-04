7786d2cc38af587a5a91fd04f06cc44947125018
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-04 16:22:45 +0100

    Refactor nav-link component to conditionally apply wire:navigate and improve class handling

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,10 +1,11 @@
-@props(['href', 'active' => false])
-<a  
-href="{{ $href }}"
-{{ $attributes->class([
-    'text-gray-300 hover:text-yellow-400',
-    'text-yellow-400 font-bold' => $active
-]) }} wire:navigate>
+@props(['active' => false, 'navigate' => true])
 
-{{ $slot }}
+@php
+$classes = $active
+    ? 'text-yellow-400 font-bold'
+    : 'text-gray-300 hover:text-yellow-400';
+@endphp
+
+<a {{ $navigate ? 'wire:navigate=""' : '' }} {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
 </a>
