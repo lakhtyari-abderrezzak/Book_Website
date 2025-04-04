5e6248efbdb07f1ceb0ba8522ec67c4e3bcb8d79
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-04 15:57:26 +0100

    Fix migration: rename 'description' field to 'body' in categories table

--- database/migrations/2025_03_15_130917_create_categories_table.php
+++ database/migrations/2025_03_15_130917_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable();
+            $table->text('body')->nullable();
             $table->string('img_path');
             $table->timestamps();
         });
