8759cf723100cdd5dfca7a40535bf69c2df54150
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-04 15:57:42 +0100

    Remove navigation component from Blade views

--- resources/views/components/nav.blade.php
+++
@@ -1,7 +0,0 @@
-
-<ul {{ $attributes->merge(['class' => ''])}}>
- <x-nav-link href="/" :active="request()->is('/') ? true : false"> Home </x-nav-link>
- <x-nav-link href="/books" :active="request()->is('books') ? true : false"> Books </x-nav-link>
- <x-nav-link href="/about" :active="request()->is('about') ? true : false"> About </x-nav-link>
- <x-nav-link href="/contact" :active="request()->is('contact') ? true : false"> Contact </x-nav-link>
-</ul>
