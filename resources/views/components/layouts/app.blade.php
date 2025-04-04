8c5c3958a08fb95e9e3c93cc65d1eea961e5c9cf
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-04 16:21:33 +0100

    Replace static navigation component with Livewire navigation menu

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -22,27 +22,7 @@
 
 
 
-    <nav x-data="{ open: false }" class="bg-gray-900 text-white p-4 shadow-md">
-        <div class="container mx-auto flex justify-between items-center">
-            <!-- Logo -->
-            <x-app-logo />
-
-            <!-- Mobile Menu Button -->
-            <button @click="open = !open" class="md:hidden focus:outline-none" aria-label="Toggle menu">
-                <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7">
-                    </path>
-                </svg>
-            </button>
-            <x-nav class="hidden md:flex space-x-6" />
-        </div>
-
-        <!-- Mobile Dropdown Menu -->
-        <div x-show="open" class="md:hidden bg-gray-800 text-white mt-2 rounded-lg shadow-lg">
-          <x-nav class="flex flex-col space-y-2 p-4" />
-        </div>
-    </nav>
+    <livewire:navigation-menu />
 
 
 
