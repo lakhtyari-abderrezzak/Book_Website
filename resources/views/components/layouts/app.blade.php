86cfd0d7bff5b72feef1359a8cc4dac493ce8faf
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-06 13:10:58 +0100

    Add mobile menu toggle functionality to app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -36,6 +36,17 @@
     </footer>
 
     @livewireScripts
+
+    <script>
+          document.addEventListener('DOMContentLoaded', () => {
+        const menuBtn = document.getElementById('mobile-menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        menuBtn.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+    });
+    </script>
 </body>
 
 </html>
