bb0b99af23c6c199b84302e564afc1a20b530eb5
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-06 13:12:20 +0100

    Update route parameter for book details to use descriptive variable

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 Route::get('books', [BookController::class , 'index'])->name('books.index');
-Route::get('books/{id}', [BookController::class , 'show'])->name('books.show');
+Route::get('books/{book}', [BookController::class , 'show'])->name('books.show');
 
 Route::get('categories/{id}', [CategoryController::class , 'show'])->name('categories.show');
 
