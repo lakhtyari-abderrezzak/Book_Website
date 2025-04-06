6dc657a212a67959e46b1eb3cca768bbf3a328c7
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-06 13:08:51 +0100

    Add description field to Book model and implement dynamic image path retrieval

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,16 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'title', 'author', 'edition', 'img_path', 'featured'
+        'title', 'author', 'edition', 'description', 'img_path', 'featured'
     ];
 
     public function Category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getImgPathAttribute($value)
+    {
+        return Str::startsWith($value, ['http://', 'https://'])
+        ? $value
+        : asset('storage/' . $value);
     }
 }
