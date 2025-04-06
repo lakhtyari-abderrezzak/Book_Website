faed31239ff5d703862e58ce134b35aedd6493e3
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-06 13:12:05 +0100

    Remove mobile menu toggle script from navigation menu view

--- resources/views/livewire/navigation-menu.blade.php
+++ resources/views/livewire/navigation-menu.blade.php
@@ -32,13 +32,3 @@
     </div>
 </nav>
 
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        const menuBtn = document.getElementById('mobile-menu-button');
-        const mobileMenu = document.getElementById('mobile-menu');
-
-        menuBtn.addEventListener('click', () => {
-            mobileMenu.classList.toggle('hidden');
-        });
-    });
-</script>
