cf427f22d64cc12a948ab51213ee913fc28ec44e
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-04-08 11:59:11 +0100

    Implement caching for featured and latest books in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {   
-        $featuredBooks = Book::where('featured', 1)->orderBy('edition', 'asc')->take(3)->get() ;
-        $latestBooks = Book::latest()->take(6)->get();
+        $featuredBooks = Cache::remember('featured_books', now()->addDay(), function () {
+            return Book::where('featured', 1)
+                ->orderBy('edition', 'asc')
+                ->take(3)
+                ->get();
+        });
+        $latestBooks = Cache::remember('latest_books', now()->addDay(), function () {
+            return Book::orderBy('created_at', 'desc')
+                ->take(6)
+                ->get();
+        });
         return view('welcome',
     [
         'featuredBooks' => $featuredBooks,
